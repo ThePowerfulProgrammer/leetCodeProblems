177a4a21f337d1c2be0cc4a197e5995f5a30a626
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-06 18:25:31 +0200

    arggg

--- pathSum/main.cpp
+++ pathSum/main.cpp
@@ -17,6 +17,54 @@ struct TreeNode {
 
 
 
+ // Determines if any nodes in a root-> leaf path adds to targetSum : true else: false
+bool hasPathSum(TreeNode * root, int targetSum)
+{
+    // seed code
+    vector<int> root_to_leaf; // all root-to-lead paths
+
+    // recursive solutions to build all paths
+
+    return createPaths(root,root_to_leaf,targetSum);
+}
+
+
+bool createPaths(TreeNode* root, vector<int>& root_to_leaf, int targetSum)
+{
+    if (root == nullptr)
+        {
+            return false;
+        }
+
+    root_to_leaf.push_back(root->val);
+
+    if (root->left == nullptr && root->right == nullptr)
+        {
+            // found a root-lead path now process
+            return checkPath(paths,targetSum);
+        }
+    else
+        {
+            // populate paths with nodes
+            createPaths(root_to_leaf->left,paths,targetSum);
+            createPaths(root_to_leaf->right,paths,targetSum);
+        }
+
+    root_to_leaf.pop_back();
+}
+
+bool checkPath(vector<int> & root_to_leaf, int targetSum)
+{
+    int sum = 0;
+
+    for (const int &i: root_to_leaf)
+        {
+            sum += i;
+        }
+    return sum == targetSum;
+}
+
+
 
 class Solution {
 public:
