6cfd78caed87fda3178b9048f441fc2f202222f3
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-07 21:48:19 +0200

    Being a lil sillly

--- pathSum/main.cpp
+++ pathSum/main.cpp
@@ -18,102 +18,28 @@ struct TreeNode {
 
 
  // Determines if any nodes in a root-> leaf path adds to targetSum : true else: false
-bool hasPathSum(TreeNode * root, int targetSum)
-{
-    // seed code
-    vector<int> root_to_leaf; // all root-to-lead paths
-
-    // recursive solutions to build all paths
-
-    return createPaths(root,root_to_leaf,targetSum);
-}
+/**
+I need to Psuedo code it :^)
 
 
-bool createPaths(TreeNode* root, vector<int>& root_to_leaf, int targetSum)
-{
-    if (root == nullptr)
-        {
-            return false;
-        }
+*/
 
-    root_to_leaf.push_back(root->val);
-
-    if (root->left == nullptr && root->right == nullptr)
-        {
-            // found a root-lead path now process
-            return checkPath(paths,targetSum);
-        }
-    else
-        {
-            // populate paths with nodes
-            createPaths(root_to_leaf->left,paths,targetSum);
-            createPaths(root_to_leaf->right,paths,targetSum);
-        }
-
-    root_to_leaf.pop_back();
-}
-
-bool checkPath(vector<int> & root_to_leaf, int targetSum)
-{
-    int sum = 0;
-
-    for (const int &i: root_to_leaf)
-        {
-            sum += i;
-        }
-    return sum == targetSum;
-}
-
-
-
-class Solution {
+ class Solution
+ {
 public:
-    void hasPathSum(TreeNode* root, int targetSum) {
-        vector<int> path;
-        printPaths(root, path, targetSum);
+    hasPathSum(TreeNode* root, int targetSum)
+    {
+
+
     }
-
-    void printPaths(TreeNode* root, vector<int>& path, int targetSum) {
-        if (root == nullptr) {
-            return;
-        }
-
-        // Append the current node's value to the path
-        path.push_back(root->val);
-
-        if (root->left == nullptr && root->right == nullptr) {
-            // Leaf node: Print the path
-            printPath(path,targetSum);
-        } else {
-            // Recurse on left and right subtrees
-            printPaths(root->left, path,targetSum);
-            printPaths(root->right, path, targetSum);
-        }
-
-        // Backtrack: Remove the last element from the path
-        path.pop_back();
-    }
-
-
 
 private:
-    void printPath(const vector<int>& path, int targetSum) {
-        for (const int &j: path) {
-            cout << j << "->";
-        }
-        cout << endl;
 
-        int sum = 0;
-        for (int i = 0; i < path.size(); ++i) {
-            sum += path[i];
-        }
+ };
 
-        if (sum == targetSum) {
-            cout << sum << endl;
-        }
 
-    }
-};
+
+
 
 int main()
 {
