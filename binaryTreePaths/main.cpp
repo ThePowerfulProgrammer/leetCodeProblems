f9247e89e4f2b8172e6c889e46c717969f85522b
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-11 22:21:16 +0200

    The Ctor

--- binaryTreePaths/main.cpp
+++ binaryTreePaths/main.cpp
@@ -28,17 +28,48 @@ class Solution
 };
 
 
+void treeTraversal(TreeNode *root)
+{
+    if (root != nullptr)
+        {
+            cout << root->val << endl;
+            treeTraversal(root->left);
+            treeTraversal(root->right);
+        }
+}
+
+void rootToLeafPaths(TreeNode *root, string nodes)
+{
+    nodes += to_string(root->val);
+    if (root->left == nullptr && root->right == nullptr)
+        {
+            // found a leaf
+            cout << nodes << endl;
+        }
+    else
+        {
+            rootToLeafPaths(root->left,nodes);
+            rootToLeafPaths(root->right, nodes);
+        }
+}
+
 int main()
 {
     cout << "Binary tree paths using recursion!" << endl;
 
     TreeNode *root = new TreeNode(4);
     root->left = new TreeNode(3);
+    root->left->right =  new TreeNode(4);
     root->left->left = new TreeNode(1);
+    root->left->left->right = new TreeNode(2);
     root->right = new TreeNode(6);
 
-    Solution s1;
-    s1.binaryTreeTraversal(root);
 
+    Solution s1;
+
+    treeTraversal(root);
+    cout << endl << endl;
+    string nodes = "";
+    rootToLeafPaths(root, nodes);
     return 0;
 }
