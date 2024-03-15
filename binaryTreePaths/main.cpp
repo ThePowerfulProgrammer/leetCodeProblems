7c33a7b078c2de486d96991822c7048436b7a4c6
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-15 21:25:44 +0200

    Getting there

--- binaryTreePaths/main.cpp
+++ binaryTreePaths/main.cpp
@@ -13,63 +13,63 @@ struct TreeNode {
  };
 
 
-class Solution
-{
-    public:
-        vector<string> binaryTreePaths(TreeNode* root)
-        {
-            return {};
-        }
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
+class Solution {
+public:
 
+    vector<string> binaryTreePaths(TreeNode* root, vector<string> path, vector<string> paths)
+    {
+        path.push_back(root->val);
 
-    private:
+        if (root->left==nullptr && root->right==nullptr)
+            {
 
+            }
+        else
+            {
+                binaryTreePaths(root->left,path,paths);
+                path.pop_back();
+            }
+    }
+    vector<string> binaryTreePaths(TreeNode* root)
+    {
+        if (root==nullptr) // we have nothing therefore we have everything. CASE 0
+            {
+                return {};
+            }
+        if (root->left == nullptr && root->right == nullptr) // if we have the root and nothing else, we have everything. CASE 1
+            {
+                string path = to_string(root->val);
+                return {path};
+            }
+        // now we return our helper function
+
+        vector<string> path = {};
+        vector<string> paths = {};
+        return binaryTreePaths(root,path, paths);
+    }
 };
 
 
-void treeTraversal(TreeNode *root)
-{
-    if (root != nullptr)
-        {
-            cout << root->val << endl;
-            treeTraversal(root->left);
-            treeTraversal(root->right);
-        }
-}
-
-void rootToLeafPaths(TreeNode *root, string nodes)
-{
-    nodes += to_string(root->val);
-    if (root->left == nullptr && root->right == nullptr)
-        {
-            // found a leaf
-            cout << nodes << endl;
-        }
-    else
-        {
-            rootToLeafPaths(root->left,nodes);
-            rootToLeafPaths(root->right, nodes);
-        }
-}
 
 int main()
 {
     cout << "Binary tree paths using recursion!" << endl;
-
-    TreeNode *root = new TreeNode(4);
-    root->left = new TreeNode(3);
-    root->left->right =  new TreeNode(4);
-    root->left->left = new TreeNode(1);
-    root->left->left->right = new TreeNode(2);
-    root->right = new TreeNode(6);
+    TreeNode *root = new TreeNode(5);
+    root->left = new TreeNode(4);
 
 
-    Solution s1;
 
-    treeTraversal(root);
-    cout << endl << endl;
-    string nodes = "";
-    rootToLeafPaths(root, nodes);
     return 0;
 }
