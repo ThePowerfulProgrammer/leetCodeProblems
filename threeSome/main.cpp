98043f524e1b5de9846881992df38abc9a58e9f2
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-23 18:42:23 +0200

    Threesome

---
+++ threeSome/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        return {{}};
+    }
+};
+
+
+/**
+
+Parameters: {4,-9,5,4,1,0,0,4}
+
+I need to determine all subsets such that the sum(subset) == 0
+
+*/
+
+
+int main()
+{
+    cout << "3 some?!" << endl;
+    return 0;
+}
