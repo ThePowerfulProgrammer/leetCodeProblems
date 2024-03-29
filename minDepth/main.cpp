089f742afb7d029ee3ee0431bf1013595535b346
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-29 20:14:58 +0200

    Binary tree? recursion

---
+++ minDepth/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
