442ef5ba79f48758e555ec0fadccd6d39e0008b9
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-04-01 21:27:30 +0200

    deFang

---
+++ deFangIPAddress/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string defangIPaddr(string address)
+    {
+        for (int i=0;i<address.size(); i++)
+            {
+                if (address[i] == '.')
+                    {
+                        address[i] = '[';
+                        address.insert(i+1,"]");
+
+                    }
+            }
+
+        return address;
+    }
+
+
+};
+
+int main()
+{
+    cout << "DeFang an IPAddress !" << endl;
+
+
+    Solution s1;
+    cout << s1.defangIPaddr("1.1.1.1") << endl;
+    cout << s1.defangIPaddr("255.100.50.0") << endl;
+    return 0;
+}
