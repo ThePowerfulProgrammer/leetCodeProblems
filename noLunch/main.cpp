8ab9e390e03ccaaf60b12a287aa8ee48cb9f14f2
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-04-19 08:22:33 +0200

    solved

--- noLunch/main.cpp
+++ noLunch/main.cpp
@@ -19,19 +19,21 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches)
     {
         int cycle = students.size();
-        int studentNumber = 0;
-        int sandwichesNumber = 0;
 
-        while (cycle > 0 || students.size() > 0)
+        while (cycle > 0)
             {
                 // student can eat
-                if (students[studentNumber] == sandwiches[sandwichesNumber])
+                if (students[0] == sandwiches[0])
                     {
                         students.erase(students.begin()); // student has food
                         sandwiches.erase(sandwiches.begin()); // food is eaten
                         cycle = students.size(); // restart the cycle
+                        if ( students.size() == 0 )
+                            {
+                                break;
+                            }
                     }
-                else if (students[studentNumber] != sandwiches[sandwichesNumber]) // student cannot eat
+                else // student cannot eat
                     {
                         int student = students[0]; // refer to student
                         cout << "student: " << student << endl;
@@ -55,6 +57,29 @@ int main()
     vector<int> students = {1,1,1,0,0,1};
     vector<int> sandwiches = {1,0,0,0,1,1};
     cout << s1.countStudents(students,sandwiches) << endl;
+
+    cout << endl << endl << endl;
+
+    for (int i=0;i<10;i++)
+        {
+            if (students[0] == sandwiches[0])
+                {
+                    students.erase(students.begin());
+                    sandwiches.erase(sandwiches.begin());
+
+                    cout << students.size() << endl;
+                    cout << sandwiches.size() << endl;
+                }
+            else
+                {
+                    int student = students[0];
+                    cout << student << endl;
+                    students.erase(students.begin());
+                    students.push_back(student);
+                    cout << students[0] << endl;
+                    cout << sandwiches[0] << endl;
+                }
+        }
 
 
     return 0;
