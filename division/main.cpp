ea4ea6e5e82af7f035b19d55614abf35af5a891e
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-09 17:37:32 +0200

    divisor/divident

---
+++ division/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, \
+and -2.7335 would be truncated to -2.
+
+Return the quotient after dividing dividend by divisor.
+
+Is this where I relearn long division? :(
+
+
+divident/divisor = quotient
+
+4/9
+*/
+
+int simpleDivison(int divident, int divisor)
+{
+
+}
+
+class Solution {
+public:
+    int divide(int dividend, int divisor)
+    {
+
+    }
+};
+
+int main()
+{
+    cout << "Recreate / " << endl;
+    int x = 6;
+    return 0;
+}
