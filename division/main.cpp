9d10445baa67c4008843b4a47958f4ac865f217d
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-10 17:17:46 +0200

    Stupid question

--- division/main.cpp
+++ division/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -39,6 +40,49 @@ int simpleDivison(int divident, int divisor)
     return quotient;
 }
 
+
+/**
+Using shift and substract method
+*/
+int longDivision(int dividend, int divisor)
+{
+
+    if (dividend == INT_MIN && divisor == -1)
+        {
+            return INT_MAX;
+        }
+
+    bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+    long positiveDividend = static_cast<long>(abs(dividend));
+    long positiveDivisor = static_cast<long>(abs(divisor));
+
+    long current = 1;
+    long denom = positiveDivisor;
+    int answer = 0;
+
+    while (positiveDividend >= denom)
+    {
+        denom <<= 1;
+        current <<= 1;
+    }
+    denom >>= 1;
+    current >>= 1;
+
+    while (current != 0)
+    {
+        if (positiveDividend >= denom)
+        {
+            positiveDividend -= denom;
+            answer |= current;
+        }
+        current >>= 1;
+        denom >>= 1;
+    }
+
+    return isNegative ? -answer : answer;
+}
+
 class Solution {
 public:
     int divide(int dividend, int divisor)
@@ -52,5 +96,6 @@ int main()
     cout << "Recreate / " << endl;
 
     cout << simpleDivison(57,6) << endl;
+    cout << longDivision(7,-3) << endl;
     return 0;
 }
